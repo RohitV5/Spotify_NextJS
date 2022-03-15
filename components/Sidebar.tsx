ee54fbc40e0c2b92fd7bea6b53cce1923fa8f5ae
Rohit Verma <verma.rohit.in@gmail.com>
2022-03-15 10:28:16 +0530

    feat: Sidebar icons buttons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,17 +1,60 @@
 import {
   HomeIcon,
-  // SearchIcon,
-  // LibraryIcon,
-  // PlusCircleIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
 } from '@heroicons/react/outline';
 
 const Sidebar = function () {
   return (
-    <div>
+    <div className="p-5 text-sm text-gray-500">
       <div>
-        <button type="button">
-          <HomeIcon className="h-5 w-5" />
+        <button
+          type="button"
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <HomeIcon className="w-5 h-5" />
+          <p>Home</p>
         </button>
+        <button
+          type="button"
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <SearchIcon className="w-5 h-5" />
+          <p>Search</p>
+        </button>
+        <button
+          type="button"
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <LibraryIcon className="w-5 h-5" />
+          <p>Library</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+        <button
+          type="button"
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <PlusCircleIcon className="w-5 h-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button
+          type="button"
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <HeartIcon className="w-5 h-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button
+          type="button"
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <RssIcon className="w-5 h-5" />
+          <p>Your episodes</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,11 @@ import Sidebar from '../components/Sidebar';
 // eslint-disable-next-line react/function-component-definition
 const Home: NextPage = () => (
   // eslint-disable-next-line prettier/prettier
-  <div className="flex flex-col items-center justify-center min-h-screen py-2">
+  <div className="">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <h1>This is a dope spotify 2.0 build</h1>
     <main>
       <Sidebar />
       {/* Center  */}
