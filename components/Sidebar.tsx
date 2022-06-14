3964c8243ec7d6afb5f415102dd5bb856bca8e93
Rohit Verma <verma.rohit.in@gmail.com>
2022-06-14 11:14:23 +0530

    feat: UseSpotify hook

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,33 +6,34 @@ import {
   HeartIcon,
   RssIcon,
 } from '@heroicons/react/outline';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 const Sidebar = function () {
-  // const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
+  console.log(session)
   return (
-    <div className="p-5 text-sm text-gray-500">
+    <div className="h-screen p-5 overflow-y-scroll text-sm text-gray-500 border-r border-gray-900 scrollbar-hide">
       <div>
         <button
           type="button"
           className="flex items-center space-x-2 hover:text-white"
           onClick={() => signOut()}
         >
-          <HomeIcon className="h-5 w-5" />
+          <HomeIcon className="w-5 h-5" />
           <p>Home</p>
         </button>
         <button
           type="button"
           className="flex items-center space-x-2 hover:text-white"
         >
-          <SearchIcon className="h-5 w-5" />
+          <SearchIcon className="w-5 h-5" />
           <p>Search</p>
         </button>
         <button
           type="button"
           className="flex items-center space-x-2 hover:text-white"
         >
-          <LibraryIcon className="h-5 w-5" />
+          <LibraryIcon className="w-5 h-5" />
           <p>Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
@@ -40,21 +41,21 @@ const Sidebar = function () {
           type="button"
           className="flex items-center space-x-2 hover:text-white"
         >
-          <PlusCircleIcon className="h-5 w-5" />
+          <PlusCircleIcon className="w-5 h-5" />
           <p>Create Playlist</p>
         </button>
         <button
           type="button"
           className="flex items-center space-x-2 hover:text-white"
         >
-          <HeartIcon className="h-5 w-5" />
+          <HeartIcon className="w-5 h-5" />
           <p>Liked Songs</p>
         </button>
         <button
           type="button"
           className="flex items-center space-x-2 hover:text-white"
         >
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="w-5 h-5" />
           <p>Your episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
